89da32c93e8278fdd2dce17267ad55da63ae15d0
kefirski <olavursky@gmail.com>
2017-10-07 07:24:45 +0300

    Some fixes

--- model/blocks/seq_to_seq.py
+++ model/blocks/seq_to_seq.py
@@ -2,12 +2,13 @@ import torch.nn as nn
 
 
 class SeqToVec(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
+    def __init__(self, input_size, hidden_size, num_layers, bidirectional):
         super(SeqToVec, self).__init__()
 
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.bidirectional = bidirectional
 
         self.rnn = nn.GRU(
             input_size=self.input_size,
@@ -20,10 +21,8 @@ class SeqToVec(nn.Module):
     def forward(self, input):
         """
         :param input: An float tensor with shape of [batch_size, seq_len, input_size]
-        :return: An float tensor with shape of [batch_size, hidden_size * 2]
+        :return: An float tensor with shape of [batch_size, seq_len, out_size]
         """
 
-        _, result = self.rnn(input)
-
-        batch_size = result.size(1)
-        return result.view(-1, 2, batch_size, self.hidden_size)[-1].transpose(0, 1).contiguous().view(batch_size, -1)
+        result, _ = self.rnn(input)
+        return result
