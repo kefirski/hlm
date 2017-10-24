2e3fbe06abb348e1c1df3766f76777140246f5e0
kefirski <olavursky@gmail.com>
2017-10-24 20:59:12 +0300

    combination fix

--- model/modules/latent/posterior_combination.py
+++ model/modules/latent/posterior_combination.py
@@ -10,39 +10,31 @@ class PosteriorCombination(nn.Module):
     def __init__(self, size):
         super(PosteriorCombination, self).__init__()
 
-        self.mu_combination = nn.Sequential(
-            weight_norm(nn.Conv1d(4, 3, kernel_size=3, stride=1, padding=2, dilation=2, bias=False)),
+        self.mu_weight = nn.Sequential(
+            weight_norm(nn.Linear(4 * size, size)),
             nn.SELU(),
 
-            ResNet(3, num_layers=2),
-
-            weight_norm(nn.Conv1d(3, 1, kernel_size=3, stride=1, padding=2, dilation=2, bias=False)),
-            nn.SELU(),
-
-            Squeeze(1),
-
-            weight_norm(nn.Linear(size, size))
+            weight_norm(nn.Linear(size, size)),
+            nn.Sigmoid()
         )
 
-        self.std_combination = nn.Sequential(
-            weight_norm(nn.Conv1d(4, 3, kernel_size=3, stride=1, padding=2, dilation=2, bias=False)),
+        self.std_weight = nn.Sequential(
+            weight_norm(nn.Linear(4 * size, size)),
             nn.SELU(),
 
-            ResNet(3, num_layers=2),
-
-            weight_norm(nn.Conv1d(3, 1, kernel_size=3, stride=1, padding=2, dilation=2, bias=False)),
-            nn.SELU(),
-
-            Squeeze(1),
-
-            weight_norm(nn.Linear(size, size))
-
+            weight_norm(nn.Linear(size, size)),
+            nn.Sigmoid()
         )
 
     def forward(self, posterior):
+        [mu_1, mu_2, std_1, std_2] = posterior
+
         input = t.stack(posterior, 1)
 
-        mu = self.mu_combination(input)
-        std = (0.5 * self.std_combination(input)).exp()
+        mu_weight = self.mu_weight(input)
+        std_weight = self.std_weight(input)
+
+        mu = mu_1 * mu_weight + mu_2 * (1 - mu_weight)
+        std = std_1 * std_weight + std_2 * (1 - std_weight)
 
         return mu, std
