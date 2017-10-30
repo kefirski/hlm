f08766ed1ee976923ed3e1307138133612071ef1
kefirski <olavursky@gmail.com>
2017-10-30 16:18:05 +0300

    Combination fix

--- model/modules/latent/posterior_combination.py
+++ model/modules/latent/posterior_combination.py
@@ -10,25 +10,27 @@ class PosteriorCombination(nn.Module):
     def __init__(self, size):
         super(PosteriorCombination, self).__init__()
 
-        self.mu_weight = nn.Sequential(
-            weight_norm(nn.Linear(4 * size, size)),
-            nn.Sigmoid()
-        )
-
-        self.std_weight = nn.Sequential(
-            weight_norm(nn.Linear(4 * size, size)),
-            nn.Sigmoid()
-        )
+        # self.mu_weight = nn.Sequential(
+        #     weight_norm(nn.Linear(4 * size, size)),
+        #     nn.Sigmoid()
+        # )
+        #
+        # self.std_weight = nn.Sequential(
+        #     weight_norm(nn.Linear(4 * size, size)),
+        #     nn.Sigmoid()
+        # )
 
     def forward(self, posterior):
         [mu_1, mu_2, std_1, std_2] = posterior
 
-        input = t.cat(posterior, 1)
+        # input = t.cat(posterior, 1)
+        #
+        # mu_weight = self.mu_weight(input)
+        # std_weight = self.std_weight(input)
+        #
+        # mu = mu_1 * mu_weight + mu_2 * (1 - mu_weight)
+        # std = std_1 * std_weight + std_2 * (1 - std_weight)
+        #
+        # return mu, std
 
-        mu_weight = self.mu_weight(input)
-        std_weight = self.std_weight(input)
-
-        mu = mu_1 * mu_weight + mu_2 * (1 - mu_weight)
-        std = std_1 * std_weight + std_2 * (1 - std_weight)
-
-        return mu, std
+        return mu_1 + mu_2, std_1 + std_2
